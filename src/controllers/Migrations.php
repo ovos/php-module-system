0ce3b8134fbe08a4e838ac128ad2da3351a5f411
Marcin Gil <mg@ovos.at>
2021-04-07 17:52:39 +0200

    php8 updates

--- src/controllers/Migrations.php
+++ src/controllers/Migrations.php
@@ -43,6 +43,7 @@ class Migrations extends Controller\Cli
 	public function __construct()
 	{
 		parent::__construct();
+		$this->setColoredOutput(true);
 		
 		$this->_paths = $this->_app->getConfig()->system->migrations;
 	}
