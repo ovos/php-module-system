74700708c8ea2e1cbbc3412bcf1ce33080824b27
Marcin Gil <mg@ovos.at>
2026-05-13 20:13:06 +0200

    Fixes DI usage in Migrations

--- application/controllers/Migrations.php
+++ application/controllers/Migrations.php
@@ -295,9 +295,9 @@ class Migrations extends Controller\Cli
 				$relativePath = substr($file->getPath(), $pathLength);
 				$namespace = str_replace('/', '\\', $relativePath);
 				$basename = $file->getBasename('.' . self::EXT);
-				$fileNameParts= explode('_', $basename);
-				$id =  array_shift($fileNameParts);
-				$filename =  implode('_', $fileNameParts);
+				$fileNameParts = explode('_', $basename);
+				$id = array_shift($fileNameParts);
+				$filename = implode('_', $fileNameParts);
 				
 				$className = 'Migrations' . $namespace . '\\' . $filename;
 				$class = new ReflectionClass($className);
